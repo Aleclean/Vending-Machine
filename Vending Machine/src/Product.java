5b1bac14b3dfa41b2c1195697b5d0103b6666262
alec.lean <alec.lean@MU-C3-24.mullen.pvt.k12.co.us>
2014-10-13 13:20:21 -0600

    Added product

--- Vending Machine/src/Product.java
+++ Vending Machine/src/Product.java
@@ -1,5 +1,22 @@
 
 public class Product
 	{
-
+	private String name;
+	private int price;
+	
+	public Product(String n, int p)
+		{
+		name = n;
+		price = p;
+		}
+	
+	public String getName()
+		{
+		return name;
+		}
+	
+	public int getPrice()
+		{
+		return price;
+		}
 	}
