6efd97258cdf8743b44cb4339416d3db96855dad
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-14 13:42:23 -0600

    Third commit

--- Vending Machine/src/VendingMachine.java
+++ Vending Machine/src/VendingMachine.java
@@ -1,26 +1,51 @@
 import java.util.Scanner;
+import java.util.io.*;
 
 public class VendingMachine
 	{
-		
-	public static void main(String[] args)
-		{
-		
+	
+	private static name;
+	private static price;
+	private static quantity;
+	
+	public static void main(String[] args) throws fileNotFoundException
+		{		
 		turnOn();
 		listen();
 		}
 	
 	public void turnOn()
 		{
-		
-		
+		initProduct();
+		System.out.println("You can use pennies, nickles, dimes, and quarters in this machine.");
+		System.out.println(" ");
 		
 		}
 	
 	public void listen()
 		{
 		Scanner keypad = new Scanner(System.in);
+		int numberOfLinesOfData = file.nextInt();
 		
+		for(int i = 0; i < numberOfLinesOfData; i ++)
+			{
+			
+			}
+		
+		}
+	
+	public void initProduct()
+		{
+		Scanner file = new Scanner (new File ("snacks.txt"));
+		int numberOfLinesOfData = file.nextInt();
+				
+				for(int i = 0; i < numberOfLinesOfData; i ++)
+					{
+					name =  file.next();
+					price = file.nextInt();
+					quantity = file.nextInt();
+					InventoryManager.addProduct();
+					}
 		}
 
 	}
