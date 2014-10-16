588fe3049cda857bff58af54ac588cbdea3376d3
Grace.Hickey <Grace.Hickey@MU-C3-22.mullen.pvt.k12.co.us>
2014-10-16 13:36:42 -0600

    a little update

--- Vending Machine/src/Cashier.java
+++ Vending Machine/src/Cashier.java
@@ -7,15 +7,6 @@ public class Cashier
 		private int showCash;
 		private int toPennies;
 	
-public Cashier(int aM, int cV, int p, int rC, int sC, int tP)
-	{
-		addMoney = aM;
-		currentValue = cV;
-		purchase = p;
-		returnChange = rC;
-		showCash= sC;
-		toPennies = tP;
-	}
 public void addMoney()
 	{
 		int value = 0;
@@ -25,10 +16,23 @@ public void currentValue()
 	{
 		int[][]value = new int[0][2];
 		System.out.println("value * 100");
+		
 	}
 public static void toPennies()
 	{
 		int[]value = new int[2];
+	}
+public void purchase()
+	{
+		
+	}
+public void showCash()
+	{
+	
+	}
+public void returnChange()
+	{
+	
 	}
 	
 }
