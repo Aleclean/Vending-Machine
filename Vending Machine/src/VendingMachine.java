f8d1a9d942e378930fb4437e49cbdc465e13e747
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-16 13:11:13 -0600

    have not done much

--- Vending Machine/src/VendingMachine.java
+++ Vending Machine/src/VendingMachine.java
@@ -4,9 +4,11 @@ import java.util.io.*;
 public class VendingMachine
 	{
 	
-	private static name;
-	private static price;
-	private static quantity;
+	private static String name;
+	private static int price;
+	private static int quantity;
+	
+	
 	
 	public static void main(String[] args) throws fileNotFoundException
 		{		
@@ -18,20 +20,14 @@ public class VendingMachine
 		{
 		initProduct();
 		System.out.println("You can use pennies, nickles, dimes, and quarters in this machine.");
-		System.out.println(" ");
+		System.out.println("A1	Cheetos	.75/n A2	Goldfish	 .75/n A3	Pretzels	.50/n A4	Jolly Ranchers	.60/n A5	Oatmeal Cookie	1.00/n B1	Snickers	.50/n B2	Stride Gum	.65/n B3	Beef Jerky	1.40/n B4 Chex Mix	.70/n B4	Popcorn	.35 ");
 		
 		}
 	
 	public void listen()
 		{
 		Scanner keypad = new Scanner(System.in);
-		int numberOfLinesOfData = file.nextInt();
-		
-		for(int i = 0; i < numberOfLinesOfData; i ++)
-			{
-			
-			}
-		
+		String choice = keypad.nextLine();		
 		}
 	
 	public void initProduct()
@@ -39,8 +35,11 @@ public class VendingMachine
 		Scanner file = new Scanner (new File ("snacks.txt"));
 		int numberOfLinesOfData = file.nextInt();
 				
-				for(int i = 0; i < numberOfLinesOfData; i ++)
+				
+		ArrayList <Product> products = new ArrayList();
+		for(int i = 0; i < numberOfLinesOfData; i ++)
 					{
+					
 					name =  file.next();
 					price = file.nextInt();
 					quantity = file.nextInt();
