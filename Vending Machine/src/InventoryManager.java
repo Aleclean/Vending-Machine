ec18b15c39f2e86d51b0297ea64c3ca21a5a39e2
alec.lean <alec.lean@MU-C3-24.mullen.pvt.k12.co.us>
2014-10-16 12:47:57 -0600

    Added managers

--- Vending Machine/src/InventoryManager.java
+++ Vending Machine/src/InventoryManager.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
 
 public class InventoryManager
 	{
-
+	ArrayList<Product> allProducts = new ArrayList();
+	
 	}
--- Vending Machine/src/ProductManager.java
+++ Vending Machine/src/ProductManager.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
 
 public class ProductManager
 	{
-
+	private Product product;
+	private int quantity;
+	private int itemCode;
+	private static String nextItemCode = "A1";
+	public ProductManager(Product p, int q)
+		{
+		product = p;
+		quantity = q;
+		}
 	}
